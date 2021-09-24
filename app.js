14aa51300fdf52715e3f4def51eb76554eb34b77
k4wt4r <kawtarelalmi0@gmail.com>
2021-09-24 11:38:47 +0100

    saving-username

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("#login-form-input");
+const loginInput = document.querySelector("#login-form-input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASS = "hidden";
 
 
-
-function onSubmit(){
-    const username = loginInput.value;
-    console.log(username);
+function onSubmit(event){
+   event.preventDefault();
+   loginForm.classList.add(HIDDEN_CLASS);
+   const username = loginInput.value;
+    localStorage.setItem("login", username);
+    greetIngs(username);
+    greeting.classList.remove(HIDDEN_CLASS);
 
 }
-loginForm.addEventListener("submit",onSubmit);
+savedUsersavedUsersavedUser
+function greetIngs(username){
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASS);
+}
+
+const savedUser = localStorage.getItem("username");
+
+if(savedUser === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+    loginForm.addEventListener("submit",onSubmit);
+} else{
+    greetIngs(savedUser);
+    
+}
